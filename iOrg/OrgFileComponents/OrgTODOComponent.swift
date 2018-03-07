2163d198d001451c7066c771d556580471479ae0
Garrett Davidson <gdavids@purdue.edu>
2018-03-07 12:19:43 -0500

    Add equality for OrgTODOComponent

--- iOrg/OrgFileComponents/OrgTODOComponent.swift
+++ iOrg/OrgFileComponents/OrgTODOComponent.swift
@@ -9,10 +9,22 @@
 import Foundation
 
 class OrgTODOComponent: OrgHeadlineComponent {
-    enum OrgTODOState {
+    enum OrgTODOState: Equatable {
         case TODO
         case DONE
         case Custom(String)
+
+        static func ==(lhs: OrgTODOComponent.OrgTODOState, rhs: OrgTODOComponent.OrgTODOState) -> Bool {
+            switch (lhs, rhs) {
+            case (.TODO, .TODO): fallthrough
+            case (.DONE, .DONE): return true
+            case (.Custom(let left), .Custom(let right)):
+                return left == right
+
+            default:
+                return false
+            }
+        }
     }
 
     var state: OrgTODOState
@@ -23,5 +35,21 @@ class OrgTODOComponent: OrgHeadlineComponent {
         self.closeDate = closeDate
 
         super.init(title: title)
+    }
+
+    override func isEqual(to other: OrgFileComponent) -> Bool {
+        guard let other = other as? OrgTODOComponent else {
+            return false
+        }
+
+        guard self.state == other.state else {
+            return false
+        }
+
+        guard self.closeDate == other.closeDate else {
+            return false
+        }
+
+        return super.isEqual(to: other)
     }
 }
