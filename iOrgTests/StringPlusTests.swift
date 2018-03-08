cc8fa68cbe3a0e0b3fc83d40d741b0e14a7f5f99
Garrett Davidson <gdavids@purdue.edu>
2018-03-07 22:22:26 -0500

    Add string extension to drop first word

---
+++ iOrgTests/StringPlusTests.swift
@@ -0,0 +1,42 @@
+//
+//  StringPlusTests.swift
+//  iOrgTests
+//
+//  Created by Garrett Davidson on 3/7/18.
+//  Copyright © 2018 Garrett Davidson. All rights reserved.
+//
+
+import XCTest
+@testable import iOrg
+
+class StringPlusTests: XCTestCase {
+    func testExample() {
+        var testString = "My test string"
+        var output = "test string"
+        XCTAssertEqual(String(testString.dropFirstWord()), output)
+
+        testString = "1 test string"
+        output = "test string"
+        XCTAssertEqual(String(testString.dropFirstWord()), output)
+
+        testString = " test string"
+        output = "string"
+        XCTAssertEqual(String(testString.dropFirstWord()), output)
+
+        testString = "string"
+        output = ""
+        XCTAssertEqual(String(testString.dropFirstWord()), output)
+
+        testString = " string"
+        output = ""
+        XCTAssertEqual(String(testString.dropFirstWord()), output)
+
+        testString = "     "
+        output = ""
+        XCTAssertEqual(String(testString.dropFirstWord()), output)
+
+        testString = ""
+        output = ""
+        XCTAssertEqual(String(testString.dropFirstWord()), output)
+    }
+}
