eadba376997f8d8038bfdceea79af0bc5aa4d1c5
Garrett Davidson <gdavids@purdue.edu>
2018-04-06 12:16:00 -0400

    Allow creating new org documents

--- iOrg/DocumentBrowserViewController.swift
+++ iOrg/DocumentBrowserViewController.swift
@@ -31,12 +31,13 @@ class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocument
     // MARK: UIDocumentBrowserViewControllerDelegate
     
     func documentBrowser(_ controller: UIDocumentBrowserViewController, didRequestDocumentCreationWithHandler importHandler: @escaping (URL?, UIDocumentBrowserViewController.ImportMode) -> Void) {
-        let newDocumentURL: URL? = nil
+
+        let newDocumentURL = Bundle.main.url(forResource: "Empty", withExtension: "org")
         
         // Set the URL for the new document here. Optionally, you can present a template chooser before calling the importHandler.
         // Make sure the importHandler is always called, even if the user cancels the creation request.
-        if newDocumentURL != nil {
-            importHandler(newDocumentURL, .move)
+        if let newDocumentURL = newDocumentURL {
+            importHandler(newDocumentURL, .copy)
         } else {
             importHandler(nil, .none)
         }
